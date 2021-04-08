fec39dabe70a1608672c11dfcdb81471539c03ef
Esteban Velardez <estebanbvelardez@gmail.com>
2021-04-08 19:53:38 -0300

    se agrego la logica para hacer el request y mostrar los datos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Frase from "./components/Frase";
-
+import {useState, useEffect, React} from"react"
 function App() {
+  //creo state
+const [personaje, setPersonaje] = useState({});
+
+useEffect(()=>{
+  //logica a ejecutar
+consultarApi()
+},[]);
+
+const consultarApi = async()=>{
+  const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+  const resultado = await respuesta.json()
+  console.log(respuesta)
+  console.log(resultado[0])
+  //Guardar objecto dentro del estate
+  setPersonaje(resultado[0])
+
+}
+
   return (
     <section className="container text-center my-5">
       <article className="d-flex flex-column align-items-center">
@@ -13,9 +30,9 @@ function App() {
           alt="logo de los simpsons"
           className='w-75'
         />
-        <Button variant="warning" className='my-5'>Agregar frase</Button>
+        <Button variant="warning" className='my-5' onClick={()=> consultarApi()} >Agregar frase</Button>
       </article>
-      <Frase></Frase>
+      <Frase personaje={personaje}></Frase>
     </section>
   );
 }
