574e86aa6c1e4955d5b946539729eeaa9560fc2d
Esteban Velardez <estebanbvelardez@gmail.com>
2021-04-12 16:54:56 -0300

    se agrego un setTimeOut en consultar api

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Frase from "./components/Frase";
 import {useState, useEffect, React} from"react"
+import  Spinner  from "./components/Spinner";
 function App() {
   //creo state
 const [personaje, setPersonaje] = useState({});
+
+const [cargando, setCargando] = useState(false)
 
 useEffect(()=>{
   //logica a ejecutar
@@ -13,14 +16,22 @@ consultarApi()
 },[]);
 
 const consultarApi = async()=>{
+  setCargando(true)
   const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
   const resultado = await respuesta.json()
   console.log(respuesta)
   console.log(resultado[0])
-  //Guardar objecto dentro del estate
-  setPersonaje(resultado[0])
 
+  setTimeout(()=>{
+    //Guardar objecto dentro del estate
+    setPersonaje(resultado[0])
+    setCargando(false)
+  },2500)
+ 
 }
+
+//operador ternario (condicion) ? (lo que quiero hacer si la condicioon es verdadera) : (lo que quiero que haga el programa si la condicion es falta)
+const mostrarComponente = (cargando === true)?<Spinner></Spinner> : <Frase personaje={personaje}></Frase>
 
   return (
     <section className="container text-center my-5">
@@ -32,7 +43,7 @@ const consultarApi = async()=>{
         />
         <Button variant="warning" className='my-5' onClick={()=> consultarApi()} >Agregar frase</Button>
       </article>
-      <Frase personaje={personaje}></Frase>
+     {mostrarComponente}
     </section>
   );
 }
